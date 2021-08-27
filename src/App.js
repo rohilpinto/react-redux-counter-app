7433da8d3b4d4ac2dfbb49649ba266d825cca738
rohilpinto <rohilcrispinto@gmail.com>
2021-08-27 14:50:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import ShowCounter from "./components/ShowCounter";
+
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./state/actions";
 
 function App() {
+  const counterState = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="counter-container">
+        <div className="content">Counter : {counterState}</div>
+        <div className="counter-btn">
+          <button onClick={() => dispatch(increment())}>+</button>
+          <button onClick={() => (counterState <= 0 ? null : dispatch(decrement()))}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/state/actions/index.js
@@ -0,0 +1,12 @@
+export const increment = (payload) => {
+  return {
+    type: "INCREMENT",
+    payload: 50,
+  };
+};
+export const decrement = (payload) => {
+  return {
+    type: "DECREMENT",
+    payload: 50,
+  };
+};
